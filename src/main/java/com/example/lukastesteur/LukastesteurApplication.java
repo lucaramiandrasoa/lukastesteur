7423ceeb8eb447e6876ca63f46133534d2a0e1bb
MAJOR <majoragency1046@gmail.com>
2024-06-01 20:46:11 +0300

    first commit

---
+++ src/main/java/com/example/lukastesteur/LukastesteurApplication.java
@@ -0,0 +1,13 @@
+package com.example.lukastesteur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukastesteurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukastesteurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lukastesteur/LukastesteurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lukastesteur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukastesteurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
